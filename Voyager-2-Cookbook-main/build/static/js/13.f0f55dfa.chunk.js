9bc5ff2477a99f170f73000403e4231556b154f4
Aditya Pareek <123439444+Aditya4289@users.noreply.github.com>
2024-01-21 13:04:04 +0530

    Delete Voyager-2-Cookbook-main directory

--- Voyager-2-Cookbook-main/build/static/js/13.f0f55dfa.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpamazon-clone"]=this["webpackJsonpamazon-clone"]||[]).push([[13],{1329:function(n,o){},1330:function(n,o){},1372:function(n,o){}}]);
-//# sourceMappingURL=13.f0f55dfa.chunk.js.map
--- Voyager-2-Cookbook-main/build/static/js/14.1996ff4e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpamazon-clone"]=this["webpackJsonpamazon-clone"]||[]).push([[14],{1329:function(n,o){},1330:function(n,o){},1335:function(n,o){}}]);
-//# sourceMappingURL=14.1996ff4e.chunk.js.map
